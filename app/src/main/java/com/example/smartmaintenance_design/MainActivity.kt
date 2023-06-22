6156eb06d93fdc0ae32313bef70d4d5c1ee6fe39
Danilo Rodrigues <danilo.o.rodrigues128@gmail.com>
2023-06-22 19:57:47 +0200

    Add random code generation

--- app/src/main/java/com/example/smartmaintenance_design/MainActivity.kt
+++ app/src/main/java/com/example/smartmaintenance_design/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.smartmaintenance_design
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var codeTV : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        codeTV = findViewById(R.id.codeTV)
+
+        val randomNums = List(6) { Random.nextInt(0, 2) }
+        var code = randomNums.map {
+            if(it == 1) {
+                Random.nextInt(65, 90).toChar()
+            } else {
+                Random.nextInt(0, 9).toString()
+            }
+        }.joinToString("")
+
+        codeTV.text = code
+        // Send code to backend
     }
 }
