18e9593c6a88fa3bebbc981ed2e01b730a23850e
pedroMunhoz <51442087+pedroMunhoz@users.noreply.github.com>
2023-06-22 22:59:42 +0200

    aaaa

---
+++ app/src/main/java/com/codewithkael/webrtcprojectforrecord/MainActivity.kt
@@ -0,0 +1,267 @@
+package com.codewithkael.webrtcprojectforrecord
+
+import android.Manifest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import com.codewithkael.webrtcprojectforrecord.databinding.ActivityMainBinding
+import com.codewithkael.webrtcprojectforrecord.models.IceCandidateModel
+import com.codewithkael.webrtcprojectforrecord.models.MessageModel
+import com.codewithkael.webrtcprojectforrecord.utils.NewMessageInterface
+import com.codewithkael.webrtcprojectforrecord.utils.PeerConnectionObserver
+import com.codewithkael.webrtcprojectforrecord.utils.RTCAudioManager
+import com.google.gson.Gson
+import com.permissionx.guolindev.PermissionX
+import org.webrtc.IceCandidate
+import org.webrtc.MediaStream
+import org.webrtc.SessionDescription
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity(), NewMessageInterface {
+
+
+    lateinit var binding : ActivityMainBinding
+    private var userName:String?=null
+    private var socketRepository:SocketRepository?=null
+    private var rtcClient : RTCClient?=null
+    private val TAG = "CallActivity"
+    private var target:String = ""
+    private val gson = Gson()
+    private var isMute = false
+    private var isCameraPause = false
+    private val rtcAudioManager by lazy { RTCAudioManager.create(this) }
+    private var isSpeakerMode = true
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val randomNums = List(6) { Random.nextInt(0, 2) }
+        var code = randomNums.map {
+            if(it == 1) {
+                Random.nextInt(65, 90).toChar()
+            } else {
+                Random.nextInt(0, 9).toString()
+            }
+        }.joinToString("")
+
+        binding.codeTV.text = code
+
+        PermissionX.init(this)
+            .permissions(
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA
+            ).request{ allGranted, _ ,_ ->
+                if (allGranted){
+                    init()
+                } else {
+                    Toast.makeText(this,"you should accept all permissions",Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    private fun init(){
+        userName = binding.codeTV.text.toString()
+        socketRepository = SocketRepository(this)
+        userName?.let { socketRepository?.initSocket(it) }
+        rtcClient = RTCClient(application,userName!!,socketRepository!!, object : PeerConnectionObserver() {
+            override fun onIceCandidate(p0: IceCandidate?) {
+                super.onIceCandidate(p0)
+                rtcClient?.addIceCandidate(p0)
+                val candidate = hashMapOf(
+                    "sdpMid" to p0?.sdpMid,
+                    "sdpMLineIndex" to p0?.sdpMLineIndex,
+                    "sdpCandidate" to p0?.sdp
+                )
+
+                socketRepository?.sendMessageToSocket(
+                    MessageModel("ice_candidate",userName,target,candidate)
+                )
+
+            }
+
+            override fun onAddStream(p0: MediaStream?) {
+                super.onAddStream(p0)
+                p0?.videoTracks?.get(0)?.addSink(binding.remoteView)
+                Log.d(TAG, "onAddStream: $p0")
+
+            }
+        })
+        rtcAudioManager.setDefaultAudioDevice(RTCAudioManager.AudioDevice.SPEAKER_PHONE)
+
+
+        binding.apply {
+            buttonExp.setOnClickListener {
+                if (buttonExp.text.toString() == "Call") {
+                    socketRepository?.sendMessageToSocket(
+                        MessageModel(
+                            "start_call", userName, targetKey.text.toString(), null
+                        )
+                    )
+                    target = targetKey.text.toString()
+                } else {
+                    targetKey.visibility = View.VISIBLE
+                    buttonExp.text = "Call"
+                }
+            }
+
+            switchCameraButton.setOnClickListener {
+                rtcClient?.switchCamera()
+            }
+
+            micButton.setOnClickListener {
+                if (isMute){
+                    isMute = false
+                    micButton.setImageResource(R.drawable.ic_baseline_mic_off_24)
+                }else{
+                    isMute = true
+                    micButton.setImageResource(R.drawable.ic_baseline_mic_24)
+                }
+                rtcClient?.toggleAudio(isMute)
+            }
+
+            videoButton.setOnClickListener {
+                if (isCameraPause){
+                    isCameraPause = false
+                    videoButton.setImageResource(R.drawable.ic_baseline_videocam_off_24)
+                }else{
+                    isCameraPause = true
+                    videoButton.setImageResource(R.drawable.ic_baseline_videocam_24)
+                }
+                rtcClient?.toggleCamera(isCameraPause)
+            }
+
+            audioOutputButton.setOnClickListener {
+                if (isSpeakerMode){
+                    isSpeakerMode = false
+                    audioOutputButton.setImageResource(R.drawable.ic_baseline_hearing_24)
+                    rtcAudioManager.setDefaultAudioDevice(RTCAudioManager.AudioDevice.EARPIECE)
+                }else{
+                    isSpeakerMode = true
+                    audioOutputButton.setImageResource(R.drawable.ic_baseline_speaker_up_24)
+                    rtcAudioManager.setDefaultAudioDevice(RTCAudioManager.AudioDevice.SPEAKER_PHONE)
+
+                }
+
+            }
+            endCallButton.setOnClickListener {
+                setCallLayoutGone()
+                setStartScreenLayoutVisible()
+                setIncomingCallLayoutGone()
+                rtcClient?.endCall()
+            }
+        }
+
+    }
+
+    override fun onNewMessage(message: MessageModel) {
+        Log.d(TAG, "onNewMessage: $message")
+        when(message.type){
+            "call_response"->{
+                if (message.data == "user is not online"){
+                    //user is not reachable
+                    runOnUiThread {
+                        Toast.makeText(this,"user is not reachable",Toast.LENGTH_LONG).show()
+
+                    }
+                }else{
+                    //we are ready for call, we started a call
+                    runOnUiThread {
+                        setStartScreenLayoutGone()
+                        setCallLayoutVisible()
+                        binding.apply {
+                            rtcClient?.initializeSurfaceView(localView)
+                            rtcClient?.initializeSurfaceView(remoteView)
+                            rtcClient?.startLocalVideo(localView)
+                            localView.visibility = View.GONE
+                            rtcClient?.call(target)
+                        }
+                    }
+
+                }
+            }
+            "answer_received" ->{
+
+                val session = SessionDescription(
+                    SessionDescription.Type.ANSWER,
+                    message.data.toString()
+                )
+                rtcClient?.onRemoteSessionReceived(session)
+                runOnUiThread {
+                    binding.remoteViewLoading.visibility = View.GONE
+                }
+            }
+            "offer_received" ->{
+                runOnUiThread {
+                    setIncomingCallLayoutVisible()
+                    binding.incomingNameTV.text = "${message.name.toString()} is calling you"
+                    binding.acceptButton.setOnClickListener {
+                        setIncomingCallLayoutGone()
+                        setCallLayoutVisible()
+                        setStartScreenLayoutGone()
+
+                        binding.apply {
+                            rtcClient?.initializeSurfaceView(localView)
+                            rtcClient?.initializeSurfaceView(remoteView)
+                            rtcClient?.startLocalVideo(localView)
+                            remoteView.visibility = View.GONE
+                        }
+                        val session = SessionDescription(
+                            SessionDescription.Type.OFFER,
+                            message.data.toString()
+                        )
+                        rtcClient?.onRemoteSessionReceived(session)
+                        rtcClient?.answer(message.name!!)
+                        target = message.name!!
+                        binding.remoteViewLoading.visibility = View.GONE
+
+                    }
+                    binding.rejectButton.setOnClickListener {
+                        setIncomingCallLayoutGone()
+                    }
+
+                }
+
+            }
+
+
+            "ice_candidate"->{
+                try {
+                    val receivingCandidate = gson.fromJson(gson.toJson(message.data),
+                        IceCandidateModel::class.java)
+                    rtcClient?.addIceCandidate(IceCandidate(receivingCandidate.sdpMid,
+                        Math.toIntExact(receivingCandidate.sdpMLineIndex.toLong()),receivingCandidate.sdpCandidate))
+                }catch (e:Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    private fun setIncomingCallLayoutGone(){
+        binding.incomingCallLayout.visibility = View.GONE
+    }
+    private fun setIncomingCallLayoutVisible() {
+        binding.incomingCallLayout.visibility = View.VISIBLE
+    }
+
+    private fun setCallLayoutGone() {
+        binding.callLayout.visibility = View.GONE
+    }
+
+    private fun setCallLayoutVisible() {
+        binding.callLayout.visibility = View.VISIBLE
+    }
+
+    private fun setStartScreenLayoutGone() {
+        binding.StartScreen.visibility = View.GONE
+    }
+
+    private fun setStartScreenLayoutVisible() {
+        binding.StartScreen.visibility = View.VISIBLE
+    }
+}
